4c378661c864c24a4e468aa605eb0f246575d835
gabtavarez <gabrieltavarescs03@gmail.com>
2026-04-15 20:01:57 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/tavarez/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,27 @@
+package com.tavarez.hrworker.repositories;
+
+import com.tavarez.hrworker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
